e3da312cfedb6db4b6c1ef3e14f9c4d521ae1e59
cantry100 <31660638+cantry100@users.noreply.github.com>
2017-09-25 01:40:43 +0800

    Revert "Add files via upload"
    This reverts commit f90b084b7b36c8d9c85b45dc272e0ed1ae901eb4.

--- YLYFindUnUsedAsset/Example/Lua/Main.lua
+++
@@ -1,8 +0,0 @@
-function OnTwist(vec2, go, deltaRotation, totalRotation)
-	
-    Shader.Find("Test/Unity/Unlit/Unlit-Texture")
-end
-
-collectgarbage("setpause", 180)
-collectgarbage("setstepmul", 300)
-
